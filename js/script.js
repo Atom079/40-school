2a84d730b9b9b211e23de2965b0a6dac1301be8a
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-01-01 03:26:04 -0800

    i have finished my work alhamdilulloh

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 'use strict'
 window.addEventListener('DOMContentLoaded', ()=>{
-    
+    const burger = document.querySelector(".bars");
+    const navLinks = document.querySelector(".nav-links")
+    console.log(navLinks);
+    burger.addEventListener('click', ()=>{
+        navLinks.classList.toggle("active_Nav");
+    })
 })
